cf06b05aa3066af042dc08834bce38dd0e96eb16
dante <hemandaney@yahoo.com>
2022-01-08 17:32:48 +0800

    cleaned up code and added main function and test runned program. Index out of range error, debug

--- gol.py
+++ gol.py
@@ -1,5 +1,5 @@
 import random
-import time
+# import time
 from copy import copy, deepcopy
 #create 2d array first to simulate a grid
 def createArray(columns, rows):
@@ -11,74 +11,86 @@ def createArray(columns, rows):
         grid.append(col)
     return grid
 
-newGrid = createArray(10,10)
-# print(newGrid)
+Grid = createArray(10,10)
+
 
 
 #computing neighbours
-def sumNeighbours(grid): 
-    newGenerationGrid = deepcopy(grid)
-    for i in range(0, len(grid)): 
+def upDateGrid(): 
+    global Grid
+    newGenerationGrid = deepcopy(Grid)
+    for i in range(0, len(Grid)): 
             sum =0
-            for x in range(0, len(grid[0])):
-                sum += grid[i-1][x]
-                sum += grid[i-1][x-1]
-                sum+= grid[i-1][x+1]
-                sum+= grid[i][x-1]
-                sum+= grid[i][x+1]
-                sum+=grid[i+1][x]
-                sum+=grid[i+1][x-1]
-                sum+=grid[i+1][x+1]
+            for x in range(0, len(Grid[0])):
+                sum += Grid[i-1][x]
+                sum += Grid[i-1][x-1]
+                sum+= Grid[i-1][x+1]
+                sum+= Grid[i][x-1]
+                sum+= Grid[i][x+1]
+                sum+=Grid[i+1][x]
+                sum+=Grid[i+1][x-1]
+                sum+=Grid[i+1][x+1]
                 #implement rules here
-                if((grid[i][x]==1 and sum<2) or(grid[i][x]==1 and sum>3)): 
+                if((Grid[i][x]==1 and sum<2) or(Grid[i][x]==1 and sum>3)): 
                     newGenerationGrid[i][x]=0
-                elif(grid[i][x]==0 and sum==3):
+                elif(Grid[i][x]==0 and sum==3):
                     newGenerationGrid[i][x]=1
                 else:
                     newGenerationGrid[i][x]=0
+    Grid = newGenerationGrid
+    
                 
-
-
-                
-
-
-
-
-
-
-#Testing basic print to console
-def printConsole(ticks): 
-    for i in range(0, ticks):
-        stringToPrint=''
-        for i in range(0, len(newGrid)): 
-            for x in range(0, len(newGrid[0])):              
-                if(newGrid[i][x]==0):
+def printGrid(): 
+    stringToPrint=''
+    global Grid
+    for i in range(0, len(Grid)): 
+            for x in range(0, len(Grid[0])):              
+                if(Grid[i][x]==0):
                     stringToPrint = stringToPrint+' '
-                    # print(' ',end ='')
                 else:
                     stringToPrint = stringToPrint+'*'
-                    # print('*',end ='')
             stringToPrint = stringToPrint +'\n'
-        print(stringToPrint, end='\r', flush=True)
-        time.sleep(0.5)
-        print(stringToPrint, end='\r', flush=True)
+    print(stringToPrint)
+
+def moveCursorToTop():
+    print("\033[12A")
+
+                
+
+# #Testing basic print to console
+# def printGrid(ticks): 
+#     for i in range(0, ticks):
+#         stringToPrint=''
+#         for i in range(0, len(newGrid)): 
+#             for x in range(0, len(newGrid[0])):              
+#                 if(newGrid[i][x]==0):
+#                     stringToPrint = stringToPrint+' '
+#                     # print(' ',end ='')
+#                 else:
+#                     stringToPrint = stringToPrint+'*'
+#                     # print('*',end ='')
+#             stringToPrint = stringToPrint +'\n'
+#         print(stringToPrint, end='\r', flush=True)
+#         time.sleep(0.5)
+#         print(stringToPrint, end='\r', flush=True)
         
 
 
-# printConsole(1) 
+def main(): 
+    ticks = int(input("Enter lifespan: "))
+    for i in range(0, ticks): 
+        printGrid()
+        moveCursorToTop()
+        upDateGrid()
+        
+
+main()
+
+
 
 
  
-print('\n* *   * * \n ** *** * \n ** *  * *\n** ***** *\n  ****    \n  * ** * *\n   ** * * \n * *** *  \n**  *  ***\n ****   * ')
-print("\033[12A")
-print('\n* *   * * \n ** *** * \n ** *  * *\n** ***** *\n  ****    \n  * ** * *\n   ** * * \n * *** *  \n**  *  ***\n ****   * ')
-time.sleep(0.5)
-   
 
-# for x in range(10):
-#     print('\n* *   * * \n ** *** * \n ** *  * *\n** ***** *\n  ****    \n  * ** * *\n   ** * * \n * *** *  \n**  *  ***\n ****   * ', end='\r')
-#     time.sleep(0.5)
-# print()            
-    
+
 
 
