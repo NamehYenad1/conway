76d720ee6fa34f657a7e93a7d860df00fc4699b1
dante <hemandaney@yahoo.com>
2022-01-08 18:48:12 +0800

    trying to make a for loop for the sum so it isnt so messy

--- gol.py
+++ gol.py
@@ -10,27 +10,17 @@ def createArray(columns, rows):
             col.append(random.randint(0,1))
         grid.append(col)
     return grid
-
 Grid = createArray(10,10)
 
 #update grid
 def upDateGrid(): 
     global Grid
     newGenerationGrid = deepcopy(Grid)
- 
-
     for i in range(0, len(Grid)): 
-            
             for x in range(0, len(Grid[0])):
                 sum =0
-                sum += Grid[(i-1+len(Grid)) % len(Grid)][(x+len(Grid)) % len(Grid[0])]
-                sum += Grid[(i-1+len(Grid))% len(Grid)][(x-1+len(Grid))% len(Grid[0])]
-                sum+= Grid[(i-1+len(Grid)) % len(Grid)][(x+1+len(Grid))% len(Grid[0])]
-                sum+= Grid[i+len(Grid) % len(Grid)][(x-1+len(Grid))% len(Grid[0])]
-                sum+= Grid[i+len(Grid) % len(Grid)][(x+1+len(Grid)) % len(Grid[0])]
-                sum+=Grid[(i+1+len(Grid)) % len(Grid)][(x+len(Grid))% len(Grid[0])]
-                sum+=Grid[(i+1+len(Grid)) % len(Grid)][(x-1+len(Grid))% len(Grid[0])]
-                sum+=Grid[(i+1+len(Grid)) % len(Grid)][(x+1+len(Grid))% len(Grid[0])]
+                for a in range(-1, 2): 
+                    sum += Grid[(i+a+len(Grid))% len(Grid)][(x+a+len(Grid))%len(Grid[0])] 
         
                 #implement rules here
                 if((Grid[i][x]==1 and sum<2) or(Grid[i][x]==1 and sum>3)): 
@@ -42,6 +32,31 @@ def upDateGrid():
 
 
     Grid = newGenerationGrid
+
+
+#SumOfNeighbours 
+
+def calculateSum():
+    
+
+    for i in range(0, len(Grid)): 
+            for x in range(0, len(Grid[0])):
+                
+                sum =0
+                sum += Grid[(i-1+len(Grid)) % len(Grid)][(x+len(Grid)) % len(Grid[0])]
+                sum += Grid[(i-1+len(Grid))% len(Grid)][(x-1+len(Grid))% len(Grid[0])]
+                sum+= Grid[(i-1+len(Grid)) % len(Grid)][(x+1+len(Grid))% len(Grid[0])]
+                sum+= Grid[i+len(Grid) % len(Grid)][(x-1+len(Grid))% len(Grid[0])]
+                sum+= Grid[i+len(Grid) % len(Grid)][(x+1+len(Grid)) % len(Grid[0])]
+                sum+=Grid[(i+1+len(Grid)) % len(Grid)][(x+len(Grid))% len(Grid[0])]
+                sum+=Grid[(i+1+len(Grid)) % len(Grid)][(x-1+len(Grid))% len(Grid[0])]
+                sum+=Grid[(i+1+len(Grid)) % len(Grid)][(x+1+len(Grid))% len(Grid[0])]
+        
+
+
+
+
+
 
     
                 
@@ -61,20 +76,21 @@ def moveCursorToTop():
     print("\033[13A")
 
                 
-def main(): 
-    ticks = int(input("Enter lifespan: "))
-    for i in range(0, ticks): 
-        print(i)
-        printGrid()
-        moveCursorToTop()
-        upDateGrid()
-        time.sleep(0.5)
+# def main(): 
+#     ticks = int(input("Enter lifespan: "))
+#     for i in range(0, ticks): 
+#         print(i)
+#         printGrid()
+#         moveCursorToTop()
+#         upDateGrid()
+#         time.sleep(0.5)
 
-main()
-
-
+# main()
 
 
+
+for i in range(-1,2):
+    print(i)
  
 
 
